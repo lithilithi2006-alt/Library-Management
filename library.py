95f84012ba52cb7e8c344f7cd218d85f2dd74bde
lithilithi2006-alt <lithilithi2006@gmail.com>
2026-01-03 16:22:05 +0530

    Added login feature

--- library.py
+++ library.py
@@ -1,1 +1,7 @@
 print("Student Registration Module")
+def login(username,password):
+    if username=="admin"and password=="1234":
+        print("Login successful")
+    else:
+        print("Login Failed")
+login("admin","1234")
